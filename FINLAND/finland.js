506ab2734f763d47ed85607b046806515f332577
JackFly26 <jackfleishman@outlook.com>
2018-04-06 21:32:56 -0500

    update index.html

--- FINLAND/finland.js
+++ FINLAND/finland.js
@@ -7,7 +7,7 @@ function preload() {
 function setup() {
   var cnv = createCanvas(720, 440);
   cnv.parent('parent');
-  background(0);
+  background(255);
   noStroke();
   scl = width / 18;
   limit = width / 18;
@@ -16,11 +16,11 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(255);
   x += 0.1;
   scl = map(sin(x), 0, 1, 0, limit);
   fill(255);
-  rect(0, 0, scl * 18, scl * 11);
+  resizeCanvas(scl * 18, scl * 11);
   fill(0, 0, 255);
   rect(0, 4 * scl, scl * 18, 3 * scl);
   rect(scl * 5, 0, scl * 3, scl * 11);
