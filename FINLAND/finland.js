effe6bf7fed495c9c5e2c1fb5ac2c8f65b1d91bf
JackFly26 <jackfleishman@outlook.com>
2018-04-07 07:38:36 -0500

    refactoring

--- FINLAND/finland.js
+++ FINLAND/finland.js
@@ -7,7 +7,6 @@ function preload() {
 function setup() {
   var cnv = createCanvas(720, 440);
   cnv.parent('parent');
-  background(255);
   noStroke();
   scl = width / 18;
   limit = width / 18;
@@ -20,7 +19,7 @@ function draw() {
   x += 0.1;
   scl = map(sin(x), 0, 1, 0, limit);
   fill(255);
-  resizeCanvas(scl * 18, scl * 11);
+  resizeCanvas(scl * 18, scl * 11, true);
   fill(0, 0, 255);
   rect(0, 4 * scl, scl * 18, 3 * scl);
   rect(scl * 5, 0, scl * 3, scl * 11);
