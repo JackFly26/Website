123cc5f2d69a943b23340d1ab189c1e79569768e
JackFly26 <jackfleishman@outlook.com>
2018-04-10 22:48:18 -0400

    Added speed slider

--- metro/sketch.js
+++ metro/sketch.js
@@ -1,7 +1,8 @@
-let turnBlack;
+let turnBlack, slider, lastSlider, int1, int2;
 
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(30, 200, 100, 1);
 }
 
 function draw() {
@@ -9,16 +10,33 @@ function draw() {
     background(0);
     turnBlack = false;
   }
+  if (lastSlider != slider.value()) {
+    clearInterval(int1);
+    clearInterval(int2);
+    int1 = setInterval(function() {
+      background(255, 0, 0);
+      setTimeout(function() {
+        turnBlack = true;
+      }, 30);
+    }, 60000 / slider.value());
+    int2 = setInterval(function() {
+      background(0, 255, 0);
+      setTimeout(function() {
+        turnBlack = true;
+      }, 30);
+    }, 60000 / slider.value() - 1);
+  }
+  lastSlider = slider.value();
 }
-setInterval(function() {
+int1 = setInterval(function() {
   background(255, 0, 0);
   setTimeout(function() {
     turnBlack = true;
   }, 30);
-}, 60000 / 100);
-setInterval(function() {
+}, 60000 / slider.value());
+int2 = setInterval(function() {
   background(0, 255, 0);
   setTimeout(function() {
     turnBlack = true;
   }, 30);
-}, 60000 / 99);
+}, 60000 / slider.value() - 1);
