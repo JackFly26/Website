a6d1f8a6daf43388f6cd836bebe13b86db19aeda
Jack Fleishman <JackFly26@unknown28C63F62DEFF.attlocal.net>
2018-04-27 19:49:18 -0400

    Working on subfolders

--- webserver.js
+++ webserver.js
@@ -1,92 +1,17 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-
+var prevreq;
 http.createServer(function(req, res) {
-    try {
-      if (req.url.length > 1) {
-        fs.readFile(req.url.replace(/\//, ''), function(err, data) {
-            if (err) {
-              console.log(err);
-              else {
-                //console.log(req.url.split(/\./)[req.url.split(/\./).length - 1]);
-                switch (req.url.split(/\./)[req.url.split(/\./).length - 1]) {
-                  case 'css':
-                    res.writeHead(200, {
-                      'Content-Type': 'text/css'
-                    });
-                    console.log('css');
-                    break;
-                  case 'js':
-                    res.writeHead(200, {
-                      'Content-Type': 'text/html'
-                    });
-                    console.log('js');
-                    break;
-                  case 'jpg':
-                    res.writeHead(200, {
-                      'Content-Type': 'image/jpeg'
-                    });
-                    console.log('jpg');
-                    break;
-                  case 'ico':
-                    res.writeHead(200, {
-                      'Content-Type': 'image/x-icon'
-                    });
-                    console.log('ico');
-                    break;
-                  case 'html':
-                    res.writeHead(200, {
-                      'Content-Type': 'text/html'
-                    });
-                    console.log('html');
-                    break;
-                  default:
-                    res.writeHead(200, {
-                      'Content-Type': 'text/plain'
-                    });
-                    break;
-                }
-                res.write(data);
-                res.end();
-              }
-            });
-        }
-        else if (req.url.split("/").length > 1) {
-          fs.readFile(req.url.replace(/\//, '') + "/index.html", function(err, data) {
-              if (err) {
-                console.log(err);
-                else {
-                  res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                  });
-                  console.log('html');
-                  res.write(data);
-                  res.end();
-                }
-              });
-          }
+  var url = req.url;
+  if (prevreq) url = prevreq + req.url; //.replace(/\//, "");
+  console.log(url);
+  try {
+    if (!/\./.test(url.split(/\//)[url.split(/\//).length - 1])) {
+      fs.readFile(url.replace(/\//, '') + "/index.html", function(err, data) {
+        if (err) {
+          console.log(err);
         } else {
-          fs.readFile('index.html', function(err, data) {
-            if (err) throw err;
-            res.writeHead(200, {
-              'Content-Type': 'text/html'
-            });
-            console.log('html');
-            if (data) res.write(data);
-            res.end();
-          });
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    });
-}
-else if (req.url.split("/").length > 1) {
-  fs.readFile(req.url.replace(/\//, '') + "/index.html", function(err, data) {
-      if (err) {
-        console.log(err);
-        else {
           res.writeHead(200, {
             'Content-Type': 'text/html'
           });
@@ -95,19 +20,67 @@ else if (req.url.split("/").length > 1) {
           res.end();
         }
       });
+    } else if (url.length > 1) {
+      fs.readFile(url.replace(/\//, ''), function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          //console.log(url.split(/\./)[url.split(/\./).length - 1]);
+          switch (url.split(/\./)[url.split(/\./).length - 1]) {
+            case 'css':
+              res.writeHead(200, {
+                'Content-Type': 'text/css'
+              });
+              console.log('css');
+              break;
+            case 'js':
+              res.writeHead(200, {
+                'Content-Type': 'text/javascript'
+              });
+              console.log('js');
+              break;
+            case 'jpg':
+              res.writeHead(200, {
+                'Content-Type': 'image/jpeg'
+              });
+              console.log('jpg');
+              break;
+            case 'ico':
+              res.writeHead(200, {
+                'Content-Type': 'image/x-icon'
+              });
+              console.log('ico');
+              break;
+            case 'html':
+              res.writeHead(200, {
+                'Content-Type': 'text/html'
+              });
+              console.log('html');
+              break;
+            default:
+              res.writeHead(200, {
+                'Content-Type': 'text/plain'
+              });
+              break;
+          }
+          res.write(data);
+          res.end();
+        }
+      });
+    } else {
+      fs.readFile('index.html', function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        console.log('html');
+        if (data) res.write(data);
+        res.end();
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
-} else {
-  fs.readFile('index.html', function(err, data) {
-    if (err) throw err;
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    console.log('html');
-    if (data) res.write(data);
-    res.end();
-  });
-}
-} catch (err) {
-  console.log(err);
-}
+  prevreq = req.url.replace(/[^\/]*\.[^\/]*$/, "");
+  console.log(prevreq);
 }).listen(80);
